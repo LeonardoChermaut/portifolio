d5b24aaac83d240465158d344b2d7bb1f732cd16
Leonardo Chermaut <chermauts@hotmail.com>
2023-02-02 20:34:11 -0300

    update: assets, containers, mixins and variables sass

--- src/components/containers/about/AboutContainer.jsx
+++ src/components/containers/about/AboutContainer.jsx
@@ -6,21 +6,24 @@ export const AboutContainer = () => {
     <section className="about-container">
       <h2>Sobre</h2>
       <p>
-        Desenvolvedor FullStack formado pelo Serratec em parceria com a SENAI
-        FIRJAN.<br></br> Sempre estive ligado a tecnologia, seja ela software
-        propriamente dito ou hardware. Que inclusive, é a minha fonte de renda
-        atual como autônomo, técnico em montagem e manutenção de hardwares em
-        geral e redes.
-        <p>
-          Por já ter trabalhado como Design Gráfico, adquiri bom gosto,
-          elegância para estilizações e na experiência do usuário, o que me
-          direcionou para o front-end.
-        </p>
-        <p>
-          Hoje o meu foco principal é terminar minha graduação em Análise e
-          Desenvolvimento de Softwares e agregar valor ao produto da sua
-          empresa.
-        </p>
+        Olá, meu nome é Leonardo e sou um desenvolvedor Full Stack apaixonado
+        por tecnologia. <br></br> Tenho 25 anos e fui formado pela parceria
+        entre o Serratec e o SENAI FIRJAN. <br></br> Desde cedo, sempre tive
+        interesse tanto em software quanto em hardware, onde atualmente, sou
+        autônomo e trabalho com montagem e manutenção de hardwares e redes.
+      </p>
+      <p>
+        Meu objetivo profissional é atuar na área de tecnologia de software,
+        especialmente no front-end. <br></br> Mas antes disso, trabalhei como
+        Design Gráfico, o que me permitiu desenvolver minhas habilidades
+        criativas e ter um bom senso de UX e estilo.
+      </p>
+      <p>
+        Sou uma pessoa altamente comunicativa, adaptável, analítica e acredito
+        que essas características foram moldadas pela minha trajetória
+        profissional, na qual trabalhoi com o público por muito tempo. <br></br>
+        E, por falar em trajetória, fiquei super animado em participar do
+        Startup Weekend Brasil, como mostro nessa minha foto de perfil xD
       </p>
       <img
         className="gif-super-mario"
--- src/components/containers/project/ProjectsContainer.jsx
+++ src/components/containers/project/ProjectsContainer.jsx
@@ -6,44 +6,37 @@ export const ProjectsContainer = () => {
     <section className="projects-container">
       <h2>Projetos</h2>
 
-      <h3 className="sub-title-project">Alterdata Open Space</h3>
-      <img
-        className="gif-dev"
-        src={gifDev}
-        alt="Desenho do super mário, sentado e programando no computador a noite."
-      />
       <div className="about-project-conteiner">
+        <h3 className="sub-title-project">Alterdata Open Space</h3>
+        <img
+          className="gif-dev"
+          src={gifDev}
+          alt="Desenho do super mário, sentado e programando no computador a noite."
+        />
         <p>
-          Onde os requisitos projeto, consistiam na criação de uma aplicação
-          "Cool Work", onde apenas os colaboradores internos da empresa teriam
-          acesso.
-          <p>
-            Previamente autorizados, a aplicação permitiria o usuário agendar o
-            dia, ou, os dias de trabalho, assim também como, os respectivos
-            horários de entrada e saída. Foi implementado uma representação
-            virtual real do ambiente interno da empresa, onde os acentos eram
-            clicáveis, dando maior acessibilidade e conforto para o usuário.
-          </p>
-          <p>
-            Após o agentamento concluído, o sistema envia um convite para os
-            emails selecionados, caso houvessem e colaborador poderia fazer as
-            seguintes ações após uma sessão cadastrada; poderia efetuar o
-            cancelamento, exclusão, edição do agendamento e pesquisa.
-          </p>
-          <p>
-            O projeto seguiu o padrão SCRUM tendo a planning, as dailys, retro e
-            a presença do Tech-Lead junto ao Product Manager.
-            <br></br>
-            Fui responsável pelo back-end em Java 11 e Spring Boot, com o banco
-            de dados Postgres.
-            <br></br>O front-end foi feito em React JS, Styled Component e
-            Material UI, onde fui revisador das estilizações, identidade visual,
-            tipografia, paletas de cores etc.
-            <br></br>A ferramenta de versionamento utilizada pela a equipe foi o
-            github, sendo um projeto privado da empresa Alterdata Software, que
-            é responsável por ser um dos patrocinadores e idealizadores do
-            projeto imersivo Serratec.
-          </p>
+          Este foi o nome criado para a aplicação de uso interno da empresa. Os
+          colaboradores previamente autorizados podem agendar seus dias e
+          horários de trabalho, tudo através de uma interface intuitiva e fácil
+          de usar.
+        </p>
+        <p>
+          O sistema representa um ambiente virtual da empresa, com cadeiras
+          clicáveis ​​para melhor acessibilidade. <br></br> Após o agendamento
+          ser concluído, os colaboradores receberam um e-mail com informações
+          sobre a sessão. <br></br>Eles também podem cancelar, excluir ou editar
+          o agendamento a qualquer momento.
+        </p>
+        <p>
+          O projeto foi supervisionado com o processo SCRUM, incluindo
+          planejamento, reuniões administrativas, retrospectiva e com a
+          liderança técnica e o gerente de produto presente.{" "}
+        </p>
+        <p>
+          Eu fiquei responsável pelo desenvolvimento do back-end com Java 11 e
+          Spring Boot, e também trabalhei na revisão do front-end feito com
+          React JS, Styled Components e Material UI.<br></br> A equipe usou o
+          Github como ferramenta de versionamento e o projeto é patrocinado pela
+          Alterdata Software.
         </p>
       </div>
     </section>
