301f18f6d1940d61a147ae4b5228399c566a840e
Leonardo Chermaut <101607060+LeonardoChermaut@users.noreply.github.com>
2023-02-05 16:29:02 -0300

    fix: direct link download cv

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -3,14 +3,14 @@ import { InformationContainer } from "../containers/information/InformationConta
 import Avatar from "../../assets/Leonardo.png";
 import "./sidebar.sass";
 
-const url = "/src/download/leonardo_chermaut_cv.pdf";
+const url = "https://drive.google.com/uc?export=download&id=1RCx_XoHKHdrx7FjPpdwqQ3a8SQyl1_EZ";
 
 export const Sidebar = () => {
   return (
     <aside id="sidebar">
       <img className="avatar-profile" src={Avatar} alt="Leonardo Chermaut" />
       <InformationContainer />
-      <a id="btn-download-portifolio" className="btn" href={url} target="_blank" download>
+      <a id="btn-download-portifolio" className="btn" href={url} target="_blank">
         Download currículo
       </a>
     </aside>
