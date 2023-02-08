ba2bf9b4e1b93217ee5ec78b4e6072743fb1c079
Leonardo Chermaut <chermauts@hotmail.com>
2023-02-07 21:02:10 -0300

    fix: url cv and replace cv on download path

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -3,15 +3,19 @@ import { InformationContainer } from "../containers/information/InformationConta
 import Avatar from "../../assets/Leonardo.png";
 import "./sidebar.sass";
 
-const url = "https://drive.google.com/uc?export=download&id=1RCx_XoHKHdrx7FjPpdwqQ3a8SQyl1_EZ";
+const url = `https://drive.google.com/uc?export=download&id=1TYTmzPTLbz-OPXRVDqYRNl0uNXSNvZXn`;
 
 export const Sidebar = () => {
   return (
     <aside id="sidebar">
-      
       <img className="avatar-profile" src={Avatar} alt="Leonardo Chermaut" />
       <InformationContainer />
-      <a id="btn-download-portifolio" className="btn" href={url} target="_blank">
+      <a
+        id="btn-download-portifolio"
+        className="btn"
+        href={url}
+        target="_blank"
+      >
         Download currículo
       </a>
     </aside>
