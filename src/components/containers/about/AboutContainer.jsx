5701ff90e031ac934dce2b3e8a97865da46a4f8b
Leonardo Chermaut <chermauts@hotmail.com>
2023-02-22 14:46:04 -0300

    adjust: assets and refactor text about me

--- src/components/containers/about/AboutContainer.jsx
+++ src/components/containers/about/AboutContainer.jsx
@@ -10,24 +10,29 @@ export const AboutContainer = () => {
       </div>
       <p>
         Olá, meu nome é Leonardo e sou um desenvolvedor Full Stack apaixonado
-        por tecnologia. <br></br> Tenho 25 anos e fui formado pela parceria
-        entre o Serratec e o SENAI FIRJAN. <br></br> Desde cedo, sempre tive
-        interesse tanto em software quanto em hardware, onde atualmente, sou
-        autônomo e trabalho com montagem e manutenção de hardwares e redes.
+        por tecnologia. Desde criança, sempre fui fascinado por hardwares,
+        softwares e isso me levou a trabalhar como autônomo em montagem,
+        manutenção de hardwares e redes.
       </p>
       <p>
-        Meu objetivo profissional é atuar na área de tecnologia de software,
-        especialmente no front-end. <br></br> Mas antes disso, trabalhei como
-        Design Gráfico, o que me permitiu desenvolver minhas habilidades
-        criativas e ter um bom senso de UX e estilo.
+        No entanto, não quero parar por aí, com o avanço, o senso de urgência,
+        em 2021 iniciei com alguns estudos em desenvolvimento,<br></br> em 2022
+        passei no processo de residência Serratec - Parque Tecnológico Região
+        Serrana, me formando como Desenvolvedor Full-Stack.<br></br> Buscando
+        sempre a constante do progresso, estou cursando Análise e
+        Desenvolvimento de Softwares pela Universidade UNIFESO. <br></br>Onde
+        meu objetivo profissional atual é focar em tecnologia de software,
+        especialmente no front-end.
       </p>
       <p>
-        Sou uma pessoa altamente comunicativa, adaptável, analítica e acredito
-        que essas características foram moldadas pela minha trajetória
-        profissional, na qual trabalhei com o público por muito tempo. <br></br>
-        E, por falar em trajetória, fiquei super animado em participar do
-        Startup Weekend Brasil, estando entre as ideias mais votadas, como
-        mostro nessa minha foto de perfil. xD
+        Já tive a oportunidade de trabalhar como Designer Gráfico em alguns
+        projetos locais da minha cidade, o que me permitiu desenvolver
+        habilidades de criatividade, UX, estilo e bom gosto.<br></br> Sou uma
+        pessoa altamente comunicativa, analítica e adaptável. Tudo isso foi
+        moldado pela minha trajetória profissional, na qual trabalhei com o
+        público por muito tempo.<br></br> <br></br>Outra coisa interessante, é
+        que participei do Startup Weekend Brasil e fiquei muito feliz por estar
+        entre as ideias mais votadas.
       </p>
       <img
         className="gif-super-mario"
