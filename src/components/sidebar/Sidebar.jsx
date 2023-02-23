88a1a008af3c0d40f94e01d828213167e335a77e
Leonardo Chermaut <101607060+LeonardoChermaut@users.noreply.github.com>
2023-02-23 13:16:57 -0300

    refac: link to download cv

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -3,7 +3,7 @@ import { InformationContainer } from "../containers/information/InformationConta
 import Avatar from "../../assets/Leonardo.png";
 import "./sidebar.sass";
 
-const url = "https://drive.google.com/uc?export=download&id=1AAOKN6VQVLLj8nBxaCjkBlelfL3QC9vu";
+const url = "https://drive.google.com/uc?export=download&id=1E_gU7B2M71SIHzqhwxbH_B9ErekqUldD";
 
 export const Sidebar = () => {
   return (
