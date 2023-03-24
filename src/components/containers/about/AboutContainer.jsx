27b3b05f3158808f5a585f933a2ec41f464c3d6e
Leonardo Chermaut <101607060+LeonardoChermaut@users.noreply.github.com>
2023-03-24 12:28:05 -0300

    refactor: text about me

--- src/components/containers/about/AboutContainer.jsx
+++ src/components/containers/about/AboutContainer.jsx
@@ -9,30 +9,20 @@ export const AboutContainer = () => {
         <h2>Sobre Mim</h2> <Bar />
       </div>
       <p>
-        Olá, meu nome é Leonardo e sou um desenvolvedor Full Stack apaixonado
-        por tecnologia. Desde criança, sempre fui fascinado por hardwares,
-        softwares e isso me levou a trabalhar como autônomo em montagem,
-        manutenção de hardwares e redes.
-      </p>
-      <p>
-        No entanto, não quero parar por aí, com o avanço, o senso de urgência,
-        em 2021 iniciei com alguns estudos em desenvolvimento,<br></br> em 2022
-        passei no processo de residência Serratec - Parque Tecnológico Região
-        Serrana, me formando como Desenvolvedor Full-Stack.<br></br> Buscando
-        sempre a constante do progresso, estou cursando Análise e
-        Desenvolvimento de Softwares pela Universidade UNIFESO. <br></br>Onde
-        meu objetivo profissional atual é focar em tecnologia de software,
-        especialmente no front-end.
-      </p>
-      <p>
-        Já tive a oportunidade de trabalhar como Designer Gráfico em alguns
-        projetos locais da minha cidade, o que me permitiu desenvolver
-        habilidades de criatividade, UX, estilo e bom gosto.<br></br> Sou uma
-        pessoa altamente comunicativa, analítica e adaptável. Tudo isso foi
-        moldado pela minha trajetória profissional, na qual trabalhei com o
-        público por muito tempo.<br></br> <br></br>Outra coisa interessante, é
-        que participei do Startup Weekend Brasil e fiquei muito feliz por estar
-        entre as ideias mais votadas.
+        Formado em desenvolvimento software Full-Stack com conhecimento técnico
+        em codificação, análise de dados e criação de códigos para o front e
+        back end. Cursando ensino superior em Tecnologia da Informação.<br></br>
+        <p>
+          Experiência em Java Spring Boot, Github com versionamento de código em
+          equipe, metodologias ágeis como SCRUM (dailys, plannings, retro etc.),
+          componentização funcional com React e React Hooks, Javascript e
+          focando em TypeScript. Prototipagem em Figma caso necessários, boas
+          noções de estilização UI UX com CSS3 FlexBox e GridLayout, Sass,
+          Styled Components, HTML5 semântico, Boostrap, MaterialUI etc.
+        </p>
+        Meu objetivo profissional é me comprometer com squads interativos,
+        dinâmicos e que priorizem a demanda em equipe, a qualidade entre os
+        colaboradores, facilitando assim o desenvolvimento de aplicativos.
       </p>
       <img
         className="gif-super-mario"
