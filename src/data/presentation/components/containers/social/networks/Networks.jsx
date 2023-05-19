71c5ad2cec2a69be9bc5a993f734200a67dbb806
Leonardo Chermaut <101607060+LeonardoChermaut@users.noreply.github.com>
2023-05-19 10:00:55 -0300

    chore: troca de tag button para acessibilidade

--- src/data/presentation/components/containers/social/networks/Networks.jsx
+++ src/data/presentation/components/containers/social/networks/Networks.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { socialNetworks } from "../constant";
+import "../socialnetwork.sass";
 
 export const Networks = () => {
     return socialNetworks.map((network) => {
         const { id: key, name, href, icon, target, className, title } = network;
         return (
-            <a
-                title={title}
+            <button
                 key={key}
                 href={href}
+                name={name}
+                title={title}
                 target={target}
                 className={className}
-                name={name}
             >
                 {icon}
-            </a>
+            </button>
         );
     })
 }
