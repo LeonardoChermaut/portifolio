86c9f0fbd8d047df3431bb502362485c5ab112cb
Leonardo Chermaut <chermauts@hotmail.com>
2023-05-31 21:09:55 -0300

    refactor: implements react.fragment na barra de carregamento

--- src/data/presentation/components/bar/Bar.jsx
+++ src/data/presentation/components/bar/Bar.jsx
@@ -3,10 +3,12 @@ import "./bar.sass"
 
 export const Bar = () => {
   return (
+    <React.Fragment>
     <div className="container">
       <div className="progress progress-striped">
         <div className="progress-bar"></div>
       </div>
     </div>
+    </React.Fragment>
   );
 };
