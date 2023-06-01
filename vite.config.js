bd473bf5d3fee93c9cab97bf77df1689aadf3305
Leonardo Chermaut <101607060+LeonardoChermaut@users.noreply.github.com>
2023-06-01 09:11:44 -0300

    chore: "atualização do vite.config com inclusão de schema server"

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    host: 'localhost',
+    strictPort: true,
+    open: true, 
+    hmr: {
+      overlay: false, 
+    },
+  },
 });
