e012c61acbf7cdf9972aaa2d6e8ff86b469977ef
Leonardo Chermaut <chermauts@hotmail.com>
2023-08-18 19:33:18 -0300

    refactor: adjust info about me

--- src/data/presentation/components/containers/about/AboutContainer.tsx
+++ src/data/presentation/components/containers/about/AboutContainer.tsx
@@ -12,18 +12,24 @@ export const AboutContainer = () => {
       <p>
         Formado em desenvolvimento software Full-Stack com conhecimento técnico
         em codificação, análise de dados e criação de códigos para o front e
-        back end. Cursando ensino superior em Tecnologia da Informação.<br></br>
-        <p>
-          Experiência em Java Spring Boot, Github com versionamento de código em
-          equipe, metodologias ágeis como SCRUM (dailys, plannings, retro etc.),
-          componentização funcional com React e React Hooks, Javascript e
-          focando em TypeScript. Prototipagem em Figma caso necessários, boas
-          noções de estilização UI UX com CSS3 FlexBox e GridLayout, Sass,
-          Styled Components, HTML5 semântico, Boostrap, MaterialUI etc.
-        </p>
-        Meu objetivo profissional é me comprometer com squads interativos,
-        dinâmicos e que priorizem a demanda em equipe, a qualidade entre os
-        colaboradores, facilitando assim o desenvolvimento de aplicativos.
+        back end. Cursando ensino superior em Tecnologia da Informação.
+      </p>
+      <p>
+        Experiência com back-end Java Spring, Git, metodologias ágeis, 
+        ReactJS e acho relevante dizer que tenho focado em aprofundar meus conhecimentos em clean code, 
+        solid, linters e principalmente no TypeScript com NestJS o que tem me agradado bastante.
+        <br></br>
+        Prototipagens com Figma ou Adobe XD caso necessários, 
+        boas noções de estilização e conhecimento em alguns dos diversos frameworks do front-end.
+      </p>
+      <p>
+          Além de Softwares, tenho background com hardwares em geral, montagem, manutenção, 
+          upgrades etc, conhecimentos sólidos com Windows, scripts, ferramentas avançadas e confortável com distros linux.
+      </p>
+      <p>
+        Hoje meu objetivo profissional é me comprometer com squads interativos, dinâmicos, 
+        que priorizem a comunicação, a demanda em equipe, a organização sobre tudo, 
+        o que facilita no desenvolvimento e no foco nas tarefas.
       </p>
       <video autoPlay loop muted
         className="video-super-mario"
