7a1cb025350180bc35985d98ccfd07fd640c842f
Leonardo Chermaut <chermauts@hotmail.com>
2023-08-19 16:19:11 -0300

    refactor: adjust and use type in map skillss

--- src/data/presentation/components/containers/information/softskills/Softskills.tsx
+++ src/data/presentation/components/containers/information/softskills/Softskills.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+type SoftskillsType = {
+    id: number,
+    name: string,
+    src: string,
+    description: string,
+    alt: string,
+}
+
 export const Softskills = ({ softskills }) => {
-    return softskills.map((skill, index) => {
-        const { name, src, description, alt } = skill;
+    return softskills.map(({id, name, src, description, alt }: SoftskillsType, _: undefined) => {
         return (
-            <div key={index} className="info-card">
+            <div key={id} className="info-card">
                 <img id={name} src={src} alt={alt} />
                 <div>
                     <p className="description">{description}</p>
@@ -18,6 +25,7 @@ export const Softskills = ({ softskills }) => {
 Softskills.propTypes = {
     softskills: PropTypes.arrayOf(
         PropTypes.shape({
+            id: PropTypes.number.isRequired,
             name: PropTypes.string.isRequired,
             src: PropTypes.string.isRequired,
             description: PropTypes.string.isRequired,
